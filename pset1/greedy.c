6e9a5d6f911bf0b8dcfc13a81205e98c127a3703
Dmytro Prokoptsov <gorshyn@gmail.com>
2017-06-26 08:01:27 +0000

    pset1_test

---
+++ pset1/greedy.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+
---
+++ pset1/water.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int x = 12;
+    printf("minutes: \n");
+    int y = GetInt();
+    printf("bottles: %i\n", x * y);
+    
+}
