52258b98dff284c6f1b2f3fa08a333d7418d7860
Dmytro Prokoptsov <gorshyn@gmail.com>
2017-06-29 13:16:42 +0000

    make greedy

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -2,3 +2,25 @@
 #include <math.h>
 #include <cs50.h>
 
+int main(void) 
+{
+    int coinsMeter = 0;
+    
+    printf("O hai! How much change is owed? \n");
+    float change = GetFloat();
+    while (change < 0) {
+        printf("How much change is owed? \n");
+        change = GetFloat();
+    }
+
+    int amount =  round(change * 100);
+    
+    while (amount > 0) {
+        if ((amount - 25) >= 0) { amount = amount - 25; coinsMeter++; }
+        else if ((amount - 10) >= 0) { amount = amount - 10; coinsMeter++; }
+        else if ((amount - 5) >= 0) { amount = amount - 5; coinsMeter++; }
+        else if ((amount - 1) >= 0) { amount = amount - 1; coinsMeter++; }
+    }
+    
+    printf("%d\n", coinsMeter);
+}
