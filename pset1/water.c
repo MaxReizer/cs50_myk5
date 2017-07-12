bbd27ebbbe3731d1fc0bf0e78c9a8859005061c1
Artem Siratskiy <siratskiy@gmail.com>
2017-07-12 19:56:43 +0300

    Added comments to the code

--- pset1/water.c
+++ pset1/water.c
@@ -1,18 +1,18 @@
-#include <cs50.h>
+#include <cs50.h> //объявление основных функций
 #include <stdio.h>
 
-int main(void)
+int main(void)  //основная функция
 {
-    printf("minutes: ");
-    int minutes = GetInt();
-    
-    if (minutes > 0)
+    printf("minutes: "); //вывод текста на экран
+    int minutes = GetInt(); //ввод кол-ва минут
+
+    if (minutes > 0) //условие кол-ва минут
     {
-        int bottles = ( 128 * (1.5 * minutes) ) / 16;
-        printf("bottles: %d\n", bottles);
+        int bottles = ( 128 * (1.5 * minutes) ) / 16; //формула подсчета минут
+        printf("bottles: %d\n", bottles); //вывод кол-ва бутылок по формуле
     }
     else
     {
-        printf("Number must be more than zero!\n");
+        printf("Number must be more than zero!\n"); //вывод ошибки неправильного ввода кол-ва минут
     }
 }
