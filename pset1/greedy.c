4efd4412074b6be1dba12106a7e2be7087fb0964
Artem Siratskiy <siratskiy@gmail.com>
2017-07-13 12:38:24 +0300

    Add comments to the code

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -13,16 +13,16 @@ int main (void) //объявление основой функции
     }
     while (total < 0);
 
-    float d = round(total * 100);
+    float d = round(total * 100); //переводим денежную сумму в целую сумму монетами
     int i = d;
     // printf("%d", i);
-    int quarters = i / 25;
-    int dimes = (i % 25) / 10;
-    int nickels = ((i % 25) % 10) / 5;
-    int pennies = (((i % 25) % 10) % 5);
-    int change = quarters + dimes + nickels + pennies;
+    int quarters = i / 25; //считаем четвертаки
+    int dimes = (i % 25) / 10;  //считаем десятки
+    int nickels = ((i % 25) % 10) / 5;  //считаем пятаки
+    int pennies = (((i % 25) % 10) % 5);  //считаем пенни
+    int change = quarters + dimes + nickels + pennies;  //считаем общее кол-во монет
 
 
-    printf("%d\n", change);
+    printf("%d\n", change); //вывод на экран кол-ва монеты
 
 }
